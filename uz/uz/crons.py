159a89b62a4214dd64a5127e4156cb4724d73af0
Voloshyn Dmytro <wdmytriy@gmail.com>
2012-08-12 23:05:32 +0300

    s

--- uz/uz/crons.py
+++ uz/uz/crons.py
@@ -1,7 +1,7 @@
 from django_cron import CronJobBase, Schedule
 import logging
 from uz.models import Entry
-
+from django.utils import timezone
 class MyCronJob(CronJobBase):
     RUN_EVERY_MINS = 5 # every 2 hours
 
@@ -9,5 +9,6 @@ class MyCronJob(CronJobBase):
     code = 'uz.my_cron_job'    # a unique code
 
     def do(self):
-        #model = Entry(train='')
+        p = Entry(train="Ogoog", pub_date=timezone.now())
+        p.save()
         logging.error("Ty huy")
