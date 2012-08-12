700a762291dff372f80daad4b320c9f49a7d3319
Voloshyn Dmytro <wdmytriy@gmail.com>
2012-08-13 01:44:20 +0300

    db changed

--- uz/uz/models.py
+++ uz/uz/models.py
@@ -2,4 +2,10 @@ from django.db import models
 
 class Entry(models.Model):
     train = models.CharField(max_length=50)
+    plaz = models.IntegerField()
+    kupe = models.IntegerField()
+    lux = models.IntegerField()
+    s1 = models.IntegerField()
+    s2 = models.IntegerField()
+    for_date = models.DateField()
     pub_date = models.DateTimeField('date published')
